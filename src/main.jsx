bf155d1bc27775b9ea3d03ce5b926476314fb6dc
7AbdoFouad <bdalrhmnfwad15@gmail.com>
2025-02-18 03:36:54 +0200

    created main project structure

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+// import './index.css'
+// document.getElementById('html').style.cssText = 'width: 100vw;'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
